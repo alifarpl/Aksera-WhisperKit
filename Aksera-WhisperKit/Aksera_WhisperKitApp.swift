d5a2ef1f144b194f7a71562e79280fba20146b99
laleaff <liffa.rr@gmail.com>
2025-10-27 09:42:00 +0700

    Initial Commit

---
+++ Aksera-WhisperKit/Aksera_WhisperKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aksera_WhisperKitApp.swift
+//  Aksera-WhisperKit
+//
+//  Created by Alifa Reppawali on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Aksera_WhisperKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
